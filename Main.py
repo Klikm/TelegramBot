9ba501df9798b4d30f75e203bbe675f750b2daf1
Klikm <50733469+Klikm@users.noreply.github.com>
2019-05-17 12:35:11 +0300

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,7 @@
+import telebot
+import requests
+
+
+URL = 'https://api.telegram.org/bot%s/873041170:AAF5yHW-U6F5kUBjwhYVE5Bl1t43XLcKzJ0'
+GitHub_URL = 'https://github.com/Klikm/TelegramBot.git'
+TOKEN = '873041170:AAF5yHW-U6F5kUBjwhYVE5Bl1t43XLcKzJ0'
